64e81711f4eba003f1446f1815d9e134f9a62b54
Brijesh Kori <73524943+Brijesh-09@users.noreply.github.com>
2024-04-10 19:16:20 +0530

    Update mainTrain.py

--- mainTrain.py
+++ mainTrain.py
@@ -10,6 +10,7 @@ from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras.utils import to_categorical
+from keras.utils.np_utils import to_categorical
 
 image_directory = 'datasets/'
 
